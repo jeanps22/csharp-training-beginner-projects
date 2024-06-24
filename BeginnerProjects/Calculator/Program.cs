20407b1f342cd654c0bff6f42e01cfb861107e35
jeanps22 <jean.philip22@hotmail.com>
2024-06-24 15:05:46 -0300

    Creating calculator project

--- BeginnerProjects/Calculator/Program.cs
+++ BeginnerProjects/Calculator/Program.cs
@@ -10,6 +10,62 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            int num1;
+            int num2;
+            string operation;
+            float result = 0;
+            bool restart = false;
+
+            Console.WriteLine("Hello, Welcome to the calculator program!");
+            do
+            {
+                restart = false;
+                Console.WriteLine("Please enter your first number.");
+                Int32.TryParse(Console.ReadLine(), out num1);
+
+                Console.WriteLine("Please enter your second number.");
+                Int32.TryParse(Console.ReadLine(), out num2);
+
+                Console.WriteLine("What type of operation would you like to do?");
+                Console.WriteLine("Plese enter + for addition, - for subtraction, * for multiplication or / for division.");
+                operation = Console.ReadLine();
+
+                switch (operation)
+                {
+                    case "+":
+                        result = num1 + num2;
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        break;
+                    case "/":
+                        if (num2 == 0)
+                        {
+                            Console.WriteLine("It is not possible to divide by zero! Try Again.");
+                            restart = true;
+                            break;
+                        }
+                        result = num1 / num2;
+                        break;
+                    default:
+                        Console.WriteLine("No operation know");
+                        restart = true;
+                        break;
+                }
+
+                if (!restart)
+                {
+                    Console.WriteLine("The result is " + result);
+                    Console.WriteLine("Do you want to continue? 'Y/N'");
+                    restart = Console.ReadLine().ToUpper() == "Y";
+                }
+            } while (restart);
+
+            Console.WriteLine("Thank you for using the calculator program!");
+            Console.ReadKey();
         }
     }
 }
