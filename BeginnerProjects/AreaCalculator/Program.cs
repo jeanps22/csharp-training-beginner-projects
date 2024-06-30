9dfdeb54ac6e3aff7f45ec00fd20083bd965f5a5
jeanps22 <jean.philip22@hotmail.com>
2024-06-30 13:22:14 -0300

    Creating Area Calculator Project

---
+++ BeginnerProjects/AreaCalculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string shape;
+            float result = 0;
+            Console.WriteLine("What shape would you like to find the area of? Please enter 'r' for rectangle or any other key for circle.");
+
+            shape = Console.ReadLine().ToLower();
+
+            switch(shape) {
+                case "r":
+                    Console.WriteLine("Please enter the height of the rectangle.");
+                    float height = float.Parse(Console.ReadLine());
+                    Console.WriteLine("Please enter the width of the rectangle.");
+                    float width = float.Parse(Console.ReadLine());
+                    result = height * width;
+                    break;
+                default:
+                    Console.WriteLine("Please enter the radius of the circle.");
+                    float radius = float.Parse(Console.ReadLine());
+                    result = (float)(Math.PI * Math.Pow(radius, 2));
+                    break;
+            }
+
+            Console.WriteLine("The result is " + result);
+            Console.ReadKey();
+        }
+    }
+}
